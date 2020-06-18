335ec726e7cb048ba6ef04c018d6f52de556413a
Bran van der Meer <bran.van.der.meer@protonmail.com>
2020-06-18 22:52:57 +0200

    feat: Initial commit; overview of kangxi radicals

---
+++ static/app.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', domContentLoaded)
+
+var doc = document
+var elem = {}
+
+function domContentLoaded() {
+  if (!__radicals) throw new Error('__radicals not defined')
+
+  cacheElements()
+  populateTable(elem.table, groupRadicals(__radicals))
+}
+
+function cacheElements() {
+  elem.layoutTable = doc.querySelector('.layout__table')
+  elem.table = doc.querySelector('.radicals-table')
+}
+
+function populateTable(table, radicals) {
+  var tbody = doc.createElement('div')
+
+  Object.entries(radicals).forEach(([strokeCount, r]) => {
+    var tr = addClass(doc.createElement('div'), 'tr')
+
+    tr.appendChild(addClass(createTextElement('div', strokeCount), 'th'))
+
+    r.forEach((r) => {
+      var td = addClass(doc.createElement('div'), 'td')
+      td.appendChild(addClass(createTextElement('div', r.pinyin), 'pinyin'))
+      td.appendChild(addClass(createTextElement('div', r.radical), 'radical'))
+      td.appendChild(addClass(createTextElement('div', r.english), 'english'))
+      tr.appendChild(td)
+    })
+
+    tbody.appendChild(tr)
+  })
+
+  table.appendChild(tbody)
+}
+
+function groupRadicals(radicals) {
+  var map = {}
+  radicals.forEach((r) => {
+    if (!map[r.strokeCount]) map[r.strokeCount] = []
+    map[r.strokeCount].push(r)
+  })
+  return map
+}
+
+function createTextElement(tag, text) {
+  var e = doc.createElement(tag)
+  e.appendChild(doc.createTextNode(text))
+  return e
+}
+
+function addClass(e, cl) {
+  e.classList.add(cl)
+  return e
+}
