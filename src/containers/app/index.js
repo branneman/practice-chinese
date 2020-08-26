8bab47e28c3eb44b9b011c5fa2bba16275cc50f6
Bran van der Meer <bran.van.der.meer@protonmail.com>
2020-08-26 13:48:07 +0200

    fix: Add baseurl to nav links

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 import RenderRoute from '../../utils/render-route'
+import pkg from '../../../package.json'
 import routes from './routes.js'
 
-import Header from '../../presenters/atoms/header'
+import Header from '../../presenters/molecules/header'
 
 import './index.scss'
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={pkg.homepage}>
       <div className="layout__container">
         <header className="layout__header">
           <Header routes={routes} />
--- src/presenters/molecules/header/index.js
+++ src/presenters/molecules/header/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
-import Logo from '../logo'
-import Nav from '../nav'
+import Logo from '../../atoms/logo'
+import Nav from '../../atoms/nav'
 
 export default function Header({ routes }) {
   return (
