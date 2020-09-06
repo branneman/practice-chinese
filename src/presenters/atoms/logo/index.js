de36114786b28cfc01f608deddc4465b192cf8be
Bran van der Meer <bran.van.der.meer@protonmail.com>
2020-09-06 17:10:55 +0200

    fix: Remove lang attributes

--- src/presenters/atoms/logo/index.js
+++ src/presenters/atoms/logo/index.js
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <figure className="logo">
       <img className="logo__svg" alt="练习中文" src="./static/img/logo.svg" />
-      <h1 className="logo__title" lang="en">
+      <h1 className="logo__title">
         <span>Practice</span> <span>Chinese</span>
       </h1>
     </figure>
