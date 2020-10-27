27b33050688bdd44165d404e8f049db2524a95b6
Bran van der Meer <bran.van.der.meer@protonmail.com>
2020-10-27 09:30:16 +0100

    feat: Add words for EUR lesson 5

--- src/data/words/eur/index.js
+++ src/data/words/eur/index.js
@@ -2,5 +2,6 @@ import { reduce, concat } from 'ramda'
 
 import l1to3 from './1-3.json'
 import l4 from './4.json'
+import l5 from './5.json'
 
-export default reduce(concat, [], [l1to3, l4])
+export default reduce(concat, [], [l1to3, l4, l5])
